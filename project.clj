6687ac17855e0da6d04a5fe947c7dad1377078d7
Lee Spector <lspector@hampshire.edu>
2014-11-08 09:58:36 -0500

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pucks "0.1.14"
+(defproject pucks "0.1.15"
   :description "An environment for experiments and education in artificial intelligence and artificial life."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
