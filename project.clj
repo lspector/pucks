f32df343210353a6c810d96b5ef7fd20efedabda
Lee Spector <lspector@hampshire.edu>
2022-01-08 22:43:48 -0500

    Bump version, make minor fixes

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject pucks "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject pucks "0.2.0"
+  :description "An environment for experiments and education in artificial intelligence and artificial life."
+  :url "https://github.com/lspector/pucks"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
